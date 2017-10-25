5279566b13ec769cba7fe1fc92552397a1e2a344
Virk22 <kulvirvirk22@gmail.com>
2017-10-24 22:37:25 -0700

    Test
    test

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+//Hello World program details for Java
+
+// two forward slashes (//) means a comment 
+// code lines after these, will not be ignored 
+
+public class HelloWorld{	
+//always start class name with Capital letter 
+// eg: HelloWorld in this class
+
+	public static void main (String[]args){
+		System.out.println("Hello world!");
+	}
+}
